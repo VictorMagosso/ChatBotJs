b56f68acc7617bbcb277237d589883d451f0f9ea
VictorMagosso <victor_magosso@hotmail.com>
2020-10-13 16:29:41 -0300

    setup com React

--- v3/backend/server.js
+++ v3/backend/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const PORT = 8080;
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
+
+app.get('/', (req, res) => {
+    req.url
+})
 
 app.post('/salvo', (req, res) => {
     console.log(req.body)
